5566bec28e3159c892ad68df8fbdfd0f26b8f27c
werty144 <anton-paramonov2000@yandex.ru>
2021-07-25 16:38:36 +0300

    text

--- app/src/main/java/com/example/novel/Scene.kt
+++ app/src/main/java/com/example/novel/Scene.kt
@@ -2,6 +2,8 @@ package com.example.novel
 
 import android.content.Context
 import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class Scene(val id: Int, val layout: ConstraintLayout, val context: Context) {
@@ -10,18 +12,34 @@ class Scene(val id: Int, val layout: ConstraintLayout, val context: Context) {
 
     val background = run {
         val backgroundId =
-            context.resources.getIdentifier(manager.idBackgorundMap[id], "drawable", "com.example.novel")
+            context.resources.getIdentifier(manager.sceneBackground(id), "drawable", "com.example.novel")
         val background = ImageView(context)
-        background.layoutParams = layout.layoutParams
+        background.layoutParams= layout.layoutParams
+        background.layoutParams.height = layout.layoutParams.width
+        background.adjustViewBounds = true
+        background.scaleType = ImageView.ScaleType.CENTER_CROP
         background.setImageResource(backgroundId)
         background
     }
 
+    val text = run {
+        val text = TextView(context)
+        text.text = manager.sceneText(id)
+        val textLayoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_PARENT, 100)
+        textLayoutParams.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID
+        textLayoutParams.bottomMargin = 10; textLayoutParams.leftMargin = 50
+        text.layoutParams = textLayoutParams
+        text.textSize = 28F
+        text
+    }
+
     fun show() {
         layout.addView(background)
+        layout.addView(text)
     }
 
     fun clear() {
         layout.removeView(background)
+        layout.removeView(text)
     }
 }
--- app/src/main/java/com/example/novel/SceneManager.kt
+++ app/src/main/java/com/example/novel/SceneManager.kt
@@ -3,6 +3,12 @@ package com.example.novel
 class SceneManager {
     val idBackgorundMap = mapOf<Int, String>(0 to "anime_tyan", 1 to "renesans_tyan")
 
+    val idTextMap = mapOf<Int, String>(0 to "Sosi huyaru", 1 to "Jizn' horosha")
+
+    fun sceneBackground(id: Int) = idBackgorundMap[id]
+
+    fun sceneText(id: Int) = idTextMap[id]
+
     fun nextId(id: Int): Int {
         return (id + 1) % 2
     }
